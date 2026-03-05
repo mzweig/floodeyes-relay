13f1f5425983d9893ea2d5a7d4bd91534f948e19
mzweig <zweigmd@gmail.com>
2026-03-05 10:19:32 -0600

    Update GS_URL to new Google Script endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.request
 import os
 
-GS_URL = "https://script.google.com/macros/s/AKfycbwiAm5Jp9dqdneNTQVQ5MmD5Yd5Wt7aXLIpXrbrosPhRLZMatvB40KqU3IP2ZLJTIS6/exec"
+GS_URL = "https://script.google.com/macros/s/AKfycbwKCkuWlxmw7DVnSHvFkMP5Ss-WcHwbDjK5D4eq2qbamBHkI1Unpaajt6kyCk5MHRRa/exec"
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
